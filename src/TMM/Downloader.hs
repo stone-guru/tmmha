d8f7f89c9387a53ced16421bd83fabbac4167389
bison <redbison@126.com>
2016-09-23 03:02:18 +0800

    little refactory Downloader.hs

--- src/TMM/Downloader.hs
+++ src/TMM/Downloader.hs
@@ -36,41 +36,58 @@ newDownloader agents =
     ax |null agents = [defaultAgent]
        |otherwise = agents
     defaultAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0"
-    
-randChoose ::Vector a -> IO a
-randChoose v = do
-  i <- randomRIO (0, (V.length v) - 1)
-  return $ v ! i
+  
 
 
 download :: Downloader -> String -> IO ResponseData
 download (Downloader manager agents loggerSet) url = do
-  agent <- randChoose agents
   req_ <- parseRequest url
-  let req = req_ { requestHeaders =
-                     [(hUserAgent, agent)
-                     ,(hAccept, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-                     ,(hAcceptLanguage, "zh,en-US;q=0.7,en;q=0.3")
-                     ,(hAcceptEncoding, "gzip, deflate, br")
-                     ,(hCacheControl, "max-age=0")
-                     ,("Upgrade-Insecure-Requests", "1")]}
+  reqHeaders <- genHeaders
+  let req = req_ {requestHeaders = reqHeaders}
+  
   response <- httpLbs req manager
+  
   let headers = responseHeaders  response
   case parseContentType $ lookup hContentType headers of
-    (False, (Just ct), _) -> return $ BinaryData ct (responseBody response)
-    (True, (Just ct), (Just cs)) -> do
-      locale <- ICU.open (b2s cs) Nothing
-      let txt :: T.Text = ICU.toUnicode locale $ B8.toStrict $ responseBody response
-      return $ TextData ct txt
+    (False, Just ctype, _) -> do
+      return $ BinaryData ctype (responseBody response)
+      
+    (True, Just ctype, charset_) -> do
+      let charset = maybe "utf-8" id charset_
+      txt <- unicoding charset $ responseBody response
+      return $ TextData ctype txt
+      
   where
     {-# INLINE parseContentType #-}
-    parseContentType :: Maybe B.ByteString -> (Bool, Maybe T.Text, Maybe B.ByteString)
-    parseContentType Nothing = (False, Just "application/octet-stream", Nothing)
-    parseContentType (Just ct) = (isText, Just (E.decodeUtf8 contentType), charset)
-      where isText = (B.isPrefixOf "text/" ct) || (B.isPrefixOf "application/js" ct)
-            contentType = fst $ B.breakSubstring ";" ct
-            charset = let rest = snd (B.breakSubstring "=" ct)
-                      in if B.null rest
-                         then Nothing
-                         else Just (B.tail rest)
+    parseContentType :: Maybe B.ByteString -> (Bool, Maybe T.Text, Maybe String)
+    parseContentType Nothing =
+      (False, Just "application/octet-stream", Nothing)
+    parseContentType (Just ct) =
+      let isText = (B.isPrefixOf "text/" ct) || (B.isPrefixOf "application/js" ct)
+          contentType = fst $ B.breakSubstring ";" ct
+          charset = let rest = snd (B.breakSubstring "=" ct)
+                    in if B.null rest then Nothing else Just $ b2s (B.tail rest)
+      in (isText, Just (E.decodeUtf8 contentType), charset)
+
+    {-# INLINE b2s #-}
     b2s = map (toEnum . fromIntegral) . B.unpack
+  
+    {-# INLINE genHeaders #-}
+    genHeaders = do
+      agent <- randChoose agents
+      return $ [(hUserAgent, agent)
+               ,(hAccept, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+               ,(hAcceptLanguage, "zh,en-US;q=0.7,en;q=0.3")
+               ,(hAcceptEncoding, "gzip, deflate, br")
+               ,(hCacheControl, "max-age=0")
+               ,("Upgrade-Insecure-Requests", "1")]
+
+    unicoding charset bytes = do
+      locale <- ICU.open charset Nothing
+      let txt = ICU.toUnicode locale $ B8.toStrict bytes
+      return txt
+      
+    {-# INLINE randChoose #-}
+    randChoose v = do
+      i <- randomRIO (0, (V.length v) - 1)
+      return $ v ! i
