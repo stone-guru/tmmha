28e7787d98d596c953f4b9771c22852d07fd2dba
bison <redbison@126.com>
2016-09-28 23:14:05 +0800

    refact select to css, avoid returning [[tag]] directly

--- src/TMM/Selector.hs
+++ src/TMM/Selector.hs
@@ -2,28 +2,26 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module TMM.Selector(
-  select,
-  select1,
+  css,
+  css1,
   extract,
   bodyText,
-  css,
-  css1
 )where
   
 import qualified Data.Text as T
+import Data.Text(Text)
 import Text.HTML.TagSoup
 import Debug.Trace
 import Control.Exception
 import Data.Maybe
 import Debug.Trace
 
-data Selector a = Fun (a -> Selector a) | OK | No
 type Matcher a b = a -> b -> Bool
 type Pred a = a -> Bool
 
-data TagCondition = Ename T.Text | Cname T.Text |EId T.Text deriving (Show)
+data TagCondition = Ename Text | Cname Text |EId Text deriving (Show)
 
-tagMatch :: Tag T.Text -> TagCondition -> Bool
+tagMatch :: Tag Text -> TagCondition -> Bool
 tagMatch (TagOpen ts _ )   (Ename ns)  =  ns == ts -- trace "#tagOpen-name" $
 tagMatch (TagOpen _ attrs) (EId ids)  =  -- trace "#tagOpen-id" $
                                          case lookup "id"  attrs of
@@ -35,7 +33,7 @@ tagMatch (TagOpen _ attrs) (Cname cs)  = -- trace "#tagOpen-class" $
                                             Just s -> elem cs (T.words s)
 tagMatch _ _ = False
 
-strToCond :: T.Text -> [TagCondition]
+strToCond :: Text -> [TagCondition]
 strToCond s = map f $ T.words s
   where
     f st = case (T.head st) of
@@ -45,19 +43,19 @@ strToCond s = map f $ T.words s
       where tl = T.tail st
 
 data Comporator a b = Comporator (Pred a) (Pred a) (Matcher a b)
-type Picker a = [Tag T.Text] -> a
+type Picker a = [Tag Text] -> a
 
 tagComporator = Comporator isTagOpen isTagClose tagMatch
 
-css ::  T.Text -> Picker a -> [Tag T.Text] -> [a]
-css str f tags = _selectb tagComporator (strToCond str) False f tags
+css ::  Text -> Picker a -> [Tag Text] -> [a]
+css str f tags = _select tagComporator (strToCond str) False f tags
 
-css1 ::  T.Text -> Picker a -> [Tag T.Text] -> [a]
-css1 str f tags = _selectb tagComporator (strToCond str) True f tags
+css1 ::  Text -> Picker a -> [Tag Text] -> [a]
+css1 str f tags = _select tagComporator (strToCond str) True f tags
 
-_selectb :: (Show a, Show b) => Comporator a b -> [b] -> Bool -> ([a] -> c) -> [a] ->[c]
-_selectb _ [] _ _ _ = error "no condition given"
-_selectb (Comporator isOpen isClose p) bx single f ax = loop [] ax
+_select :: (Show a, Show b) => Comporator a b -> [b] -> Bool -> ([a] -> c) -> [a] ->[c]
+_select _ [] _ _ _ = error "no condition given"
+_select (Comporator isOpen isClose p) bx single f ax = loop [] ax
   where
     bxv = reverse bx
 
@@ -77,16 +75,12 @@ _selectb (Comporator isOpen isClose p) bx single f ax = loop [] ax
       | otherwise = search stack ax
 
 
-select :: T.Text -> [Tag T.Text] -> [[Tag T.Text]]
-select str tags = _select (strToCond str) isTagOpen isTagClose tagMatch tags
+extract :: Text -> [Tag Text] -> Text
+extract str tags = case css1 str bodyText tags of
+                     t:_ -> t
+                     [] -> T.empty
 
-select1 :: T.Text -> [Tag T.Text] -> [Tag T.Text]
-select1 str tags = head $ select str tags
-
-extract :: T.Text -> [Tag T.Text] -> T.Text
-extract str tags = bodyText $ select1 str tags
-
-bodyText :: [Tag T.Text] -> T.Text
+bodyText :: [Tag Text] -> Text
 bodyText [] = T.empty
 bodyText (t:tx) = T.concat $! loop [t] tx
   where
@@ -97,27 +91,6 @@ bodyText (t:tx) = T.concat $! loop [t] tx
       |isTagOpen t = loop (t:s:sx) tx
       |isTagClose t = loop sx tx
       |otherwise = loop (s:sx) tx
-
-_select :: (Show a, Show b) => [b] -> Pred a -> Pred a -> Matcher a b -> [a] ->[[a]]
-_select [] _ _ _ _ = error "no condition given"
-_select bx isOpen isClose p ax = loop [] ax
-  where
-    bxv = reverse bx
-
-    loop !sx !ax = case search sx ax of
-                   Nothing-> []
-                   Just (stack, t:ts) -> (t:ts) : loop (t:stack) ts
-
-    search _ [] = Nothing
-    search !stack !rest@(a:ax)
-      | isOpen a =  -- trace ((show $ a:stack) ++ ", " ++ (show bxv)) $
-                   if match p (a:stack) bxv 
-                   then Just (stack, rest)
-                   else search (a:stack) ax
-      | isClose a = case stack of
-                      _:sx -> search sx ax
-                      [] -> error "unbalanced sequence"
-      | otherwise = search stack ax
 
 {-# INLINE match #-}
 match :: Matcher a b -> [a] -> [b] -> Bool
@@ -133,3 +106,25 @@ match p (a:ax) (b:bx)
                               [] -> False
                               _:rest -> lookupper rest bx
                                      
+
+data Regexy = StartsWith
+            |   EndsWith
+            |   Contains
+            |   Equals
+            |   Anything
+            |   ContainsWord
+            |   NotEquals
+            deriving (Eq, Show)
+
+data Selector =  Any
+              |  Type Text
+              |  Id Text
+              |  Class Text
+              |  Attribute  Regexy Text Text
+              |  And [Selector]
+              |  Or  [Selector]
+              |  Not [Selector]
+              |  Has [Selector]
+              |  ParentIs Selector
+              |  AncestorIs Selector
+              
